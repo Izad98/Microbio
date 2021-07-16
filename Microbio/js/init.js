044c24853c0326eb4b19126a66c0c789d82b8e33
Izad98 <73217654+Izad98@users.noreply.github.com>
2021-07-16 07:34:05 +0530

    Update init.js

--- Microbio/js/init.js
+++ Microbio/js/init.js
@@ -26,6 +26,9 @@ var windowsHeight = $(window).height();
         "images/bg1.jpg"
       , "images/bg2.jpg"
       , "images/bg3.jpg"
+      , "images/bg4.jpg"
+      , "images/bg5.jpg"
+      , "images/bg6.jpg"
     ], {duration: 3000, fade: 750});
 
 jQuery(document).ready(function(){
